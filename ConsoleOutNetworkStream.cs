1a16d63373ec6ff0956b01f06eb90b5edf5918d0
isotface <58082694+isotface@users.noreply.github.com>
2019-11-23 09:48:00 +0900

    Add files via upload

---
+++ ConsoleOutNetworkStream.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MyUtil
+{
+	class ConsoleOutNetworkStream
+	{
+		public static void main()
+		{
+			System.Net.Sockets.Socket sock = new System.Net.Sockets.Socket(
+				System.Net.Sockets.AddressFamily.InterNetwork,
+				System.Net.Sockets.SocketType.Stream,
+				System.Net.Sockets.ProtocolType.Tcp);
+			System.Net.IPEndPoint ipEndPt = new System.Net.IPEndPoint(
+				new System.Net.IPAddress(new byte[] { 127, 0, 0, 1 }), 20000);
+			sock.Connect(ipEndPt);
+
+			if (!sock.Connected)
+				Console.WriteLine("Not Connected");
+
+			System.Net.Sockets.NetworkStream ns = new System.Net.Sockets.NetworkStream(sock);
+
+			System.IO.StreamWriter sw = new System.IO.StreamWriter(ns);
+			sw.AutoFlush = true;		// trueにしないと出力が即時反映されない
+			System.IO.TextWriter tw = System.IO.TextWriter.Synchronized(sw);
+			Console.SetOut(tw);
+
+			Console.WriteLine("Start");
+			Console.WriteLine("Test");
+			//sw.WriteLine("Test");
+			Console.WriteLine("Test2");
+			//sw.WriteLine("Test");
+			Console.WriteLine("Test3");
+			//sw.WriteLine("Test3");
+
+			while (true)
+			{
+				string dt = DateTime.Now.ToString("yyyyMMdd HH:mm:ss.fff");
+				Console.WriteLine(dt);
+				//sw.WriteLine(dt);
+				//sw.Flush();
+				//byte[] buff = System.Text.Encoding.ASCII.GetBytes(dt + Environment.NewLine);
+				//ns.Write(buff, 0, buff.Length);
+				System.Threading.Thread.Sleep(1000);
+			}
+		}
+	}
+}
--- MyHelper.h
+++ MyHelper.h
@@ -1,26 +1,48 @@
 #pragma once
-#include <afxwin.h>
+
+#include "stdafx.h"
 
 class CMyHelper
 {
 public:
 	CMyHelper();
 	~CMyHelper();
-	static int WriteText(CEdit &cEdit, CString &cStrText, BOOL bAppend = TRUE, BOOL bNewLine = TRUE);
-	static int WriteText(CEdit &cEdit, LPCTSTR lpszText, BOOL bAppend = TRUE, BOOL bNewLine = TRUE);
-	static int WriteText(CWnd* cWnd, int nID, CString &cStrText, BOOL bAppend = TRUE, BOOL bNewLine = TRUE);
-	static int WriteText(CWnd* cWnd, int nID, LPCTSTR lpszText, BOOL bAppend = TRUE, BOOL bNewLine = TRUE);
 
 	static int GetRadioValue(CButton* acRadio[], int count);
 	static int GetRadioValue(CWnd* cWnd, int anID[], int count);
 
+	static int CEditWriteText(CEdit &cEdit, CString &cStrText, BOOL bAppend = TRUE, BOOL bNewLine = TRUE);
+	static int CEditWriteText(CEdit &cEdit, LPCTSTR lpszText, BOOL bAppend = TRUE, BOOL bNewLine = TRUE);
+	static int CEditWriteText(CWnd* cWnd, int nID, CString &cStrText, BOOL bAppend = TRUE, BOOL bNewLine = TRUE);
+	static int CEditWriteText(CWnd* cWnd, int nID, LPCTSTR lpszText, BOOL bAppend = TRUE, BOOL bNewLine = TRUE);
+
 private:
-	static int writeText(CEdit &cEdit, CString &cStrText, BOOL bAppend, BOOL bNewLine);
-	static int writeText(CWnd* cWnd, int nID, CString &cStrText, BOOL bAppend, BOOL bNewLine);
+	static int ceditWriteText(CEdit &cEdit, CString &cStrText, BOOL bAppend, BOOL bNewLine);
+	static int ceditWriteText(CWnd* cWnd, int nID, CString &cStrText, BOOL bAppend, BOOL bNewLine);
+
+public:
+	static int OpenFileDialog(CStringArray &cStrArrPath, BOOL bMulti = FALSE, int nFiles = 99);
+	static CString Debug_ByteStr(void* ptr, int nByteLen);
+	static int CalcBCC(BYTE* pbyData, int nLen);
+
 
 };
 
 
+CMyHelper::CMyHelper()
+{
+	ASSERT(FALSE);		// new‹ÖŽ~
+}
+
+
+CMyHelper::~CMyHelper()
+{
+}
+
+
+//
+//
+//
 int CMyHelper::GetRadioValue(CButton* apcRadio[], int count)
 {
 	int ret = -1;
@@ -41,6 +63,9 @@ int CMyHelper::GetRadioValue(CButton* apcRadio[], int count)
 
 	return ret;
 }
+//
+//
+//
 int CMyHelper::GetRadioValue(CWnd* cWnd, int anID[], int count)
 {
 	int ret = -1;
@@ -66,35 +91,29 @@ int CMyHelper::GetRadioValue(CWnd* cWnd, int anID[], int count)
 }
 
 
-CMyHelper::CMyHelper()
+//
+//
+//
+int CMyHelper::CEditWriteText(CEdit &cEdit, CString &cStrText, BOOL bAppend/*=TRUE*/, BOOL bNewLine/*=TRUE*/)
 {
+	return ceditWriteText(cEdit, cStrText, bAppend, bNewLine);
 }
-
-
-CMyHelper::~CMyHelper()
+int CMyHelper::CEditWriteText(CEdit &cEdit, LPCTSTR lpszText, BOOL bAppend/*=TRUE*/, BOOL bNewLine/*=TRUE*/)
 {
+	return ceditWriteText(cEdit, CString(lpszText), bAppend, bNewLine);
 }
-
-
-int CMyHelper::WriteText(CEdit &cEdit, CString &cStrText, BOOL bAppend/*=TRUE*/, BOOL bNewLine/*=TRUE*/)
+int CMyHelper::CEditWriteText(CWnd* cWnd, int nID, CString &cStrText, BOOL bAppend/*=TRUE*/, BOOL bNewLine/*=TRUE*/)
 {
-	return writeText(cEdit, cStrText, bAppend, bNewLine);
+	return ceditWriteText(cWnd, nID, cStrText, bAppend, bNewLine);
 }
-int CMyHelper::WriteText(CEdit &cEdit, LPCTSTR lpszText, BOOL bAppend/*=TRUE*/, BOOL bNewLine/*=TRUE*/)
+int CMyHelper::CEditWriteText(CWnd* cWnd, int nID, LPCTSTR lpszText, BOOL bAppend/*=TRUE*/, BOOL bNewLine/*=TRUE*/)
 {
-	return writeText(cEdit, CString(lpszText), bAppend, bNewLine);
+	return ceditWriteText(cWnd, nID, CString(lpszText), bAppend, bNewLine);
 }
-int CMyHelper::WriteText(CWnd* cWnd, int nID, CString &cStrText, BOOL bAppend/*=TRUE*/, BOOL bNewLine/*=TRUE*/)
-{
-	return writeText(cWnd, nID, cStrText, bAppend, bNewLine);
-}
-int CMyHelper::WriteText(CWnd* cWnd, int nID, LPCTSTR lpszText, BOOL bAppend/*=TRUE*/, BOOL bNewLine/*=TRUE*/)
-{
-	return writeText(cWnd, nID, CString(lpszText), bAppend, bNewLine);
-}
-
-
-int CMyHelper::writeText(CEdit &cEdit, CString &cStrText, BOOL bAppend, BOOL bNewLine)
+//
+//
+//
+int CMyHelper::ceditWriteText(CEdit &cEdit, CString &cStrText, BOOL bAppend, BOOL bNewLine)
 {
 	int ret = 0;
 
@@ -114,9 +133,10 @@ int CMyHelper::writeText(CEdit &cEdit, CString &cStrText, BOOL bAppend, BOOL bNe
 
 	return ret;
 }
-
-
-int CMyHelper::writeText(CWnd* cWnd, int nID, CString &cStrText, BOOL bAppend, BOOL bNewLine)
+//
+//
+//
+int CMyHelper::ceditWriteText(CWnd* cWnd, int nID, CString &cStrText, BOOL bAppend, BOOL bNewLine)
 {
 	if (cWnd == NULL) {
 		return -1;
@@ -145,4 +165,117 @@ int CMyHelper::writeText(CWnd* cWnd, int nID, CString &cStrText, BOOL bAppend, B
 	pcEdit->SetWindowText(cStrWork);
 
 	return ret;
+}
+
+
+//
+int CMyHelper::OpenFileDialog(CStringArray &cStrArrPath, BOOL bMulti /*=FALSE*/, int nFiles /*=99*/)
+{
+	//const int MAX_CFileDialog_FILE_COUNT = 99;
+	const int FILE_LIST_BUFFER_SIZE = ((nFiles * (MAX_PATH + 1) + 1));
+	CString cStrTmp;
+	CString cStrDir;
+
+	TCHAR* pc;
+	TCHAR* pcBuffEnd;
+	TCHAR* pcBuffStart;
+
+	CFileDialog cFileDlg(TRUE);
+	OPENFILENAME& stOFN = cFileDlg.GetOFN();
+
+	//TRY
+	//{
+		pc = cStrTmp.GetBuffer(FILE_LIST_BUFFER_SIZE);
+		if (bMulti) {
+			stOFN.Flags |= OFN_ALLOWMULTISELECT;
+		}
+		stOFN.lpstrFile = pc;
+		stOFN.nMaxFile = FILE_LIST_BUFFER_SIZE;
+
+		cFileDlg.DoModal();
+		cStrTmp.ReleaseBuffer();
+
+		pcBuffEnd = pc + FILE_LIST_BUFFER_SIZE - 2;
+		pcBuffStart = pc;
+
+		//TRACE("%s", (LPCTSTR)Debug_ByteStr(pc, FILE_LIST_BUFFER_SIZE - 2));
+
+		while ((pc < pcBuffEnd) && (*pc)) {
+			pc++;
+		}
+		if (pc > pcBuffStart) {
+			cStrDir.Format(_T("%s"), pcBuffStart);
+			pc++;
+
+			while ((pc < pcBuffEnd) && (*pc)) {
+				pcBuffStart = pc;
+
+				while ((pc < pcBuffEnd) && (*pc)) {
+					pc++;
+				}
+				if (pc > pcBuffStart) {
+					cStrTmp.Format(_T("%s\\%s"), cStrDir, pcBuffStart);
+					cStrArrPath.Add(cStrTmp);
+				}
+				pc++;
+			}
+		}
+
+		if (cStrArrPath.GetCount() <= 0 && 0 < cStrDir.GetLength()) {
+			cStrArrPath.Add(cStrDir);
+		}
+
+		TRACE("cStrArrPath.GetCount()=%d", cStrArrPath.GetCount());
+	//}
+	//CATCH(CException, pcEx)
+	//{
+	//	pcEx->ReportError();
+	//	return -1;
+	//}
+	//END_CATCH
+
+	return cStrArrPath.GetCount();
+}
+
+
+//#define		HEX2CHR(c)		((0xA <= (c)) ? ((c) + 0x37) : ((c) + 0x30))
+
+//
+//
+//
+CString CMyHelper::Debug_ByteStr(void* ptr, int nByteLen)
+{
+	if (ptr == NULL) {
+		return CString("");
+	}
+
+	CString cStr = "";
+	BYTE* p = (BYTE*)ptr;
+	//TCHAR buff[] = { 0, 0, 0, 0 };
+	TCHAR buff[4] = {};
+
+	for (int i = 0; i < nByteLen; i++) {
+		//buff[0] = HEX2CHR((*(p + i) >> 4) & 0xF);
+		//buff[1] = HEX2CHR(*(p + i) & 0xF);
+		snprintf(buff, sizeof(buff), "%02X ", *(p + i));
+		cStr += CString(buff);
+	}
+	return cStr;
+}
+
+
+//
+//
+//
+int CMyHelper::CalcBCC(BYTE* pbyData, int nLen)
+{
+	if (pbyData == NULL) {
+		return 0;
+	}
+
+	BYTE by = 0;
+	for (int i = 0; i < nLen; i++) {
+		by ^= *(pbyData + i);
+	}
+	return by;
 }
