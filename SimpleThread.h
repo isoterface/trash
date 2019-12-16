2bbae1b5e6bf1c226961ed0ac16789f71985229d
isotface <58082694+isotface@users.noreply.github.com>
2019-12-17 04:17:50 +0900

    Add files via upload

--- SimpleThread.h
+++ SimpleThread.h
@@ -19,19 +19,19 @@ public:
 	CThread();
 	~CThread();
 	
-	BOOL SetArgs(LPSECURITY_ATTRIBUTES lpThreadAttributes,
-		DWORD dwStackSize,
-		LPTHREAD_START_ROUTINE lpStartAddress,
-		LPVOID lpParameter,
-		DWORD dwCreationFlags);
-	BOOL Start();
-	BOOL Join();
-	static BOOL JoinAll(CThread acThread[], int nCount);
+	BOOL					SetParameter(LPSECURITY_ATTRIBUTES lpThreadAttributes,
+								DWORD dwStackSize,
+								LPTHREAD_START_ROUTINE lpStartAddress,
+								LPVOID lpParameter,
+								DWORD dwCreationFlags);
+	BOOL					SetThreadProc(LPTHREAD_START_ROUTINE lpStartAddress);
+	BOOL					SetThreadParam(LPVOID lpParameter);
 
-	BOOL SetFunction(LPTHREAD_START_ROUTINE lpStartAddress);
-	BOOL SetParameter(LPVOID lpParameter);
+	BOOL					Start();
+	BOOL					Join(DWORD dwWait = INFINITE);
+	static BOOL				JoinAll(CThread acThread[], int nCount, DWORD dwWait = INFINITE);
 
-	HANDLE GetHandle();
+	HANDLE					GetHandle();
 };
 
 
@@ -54,31 +54,25 @@ CThread::~CThread()
 }
 
 
-BOOL CThread::SetArgs(LPSECURITY_ATTRIBUTES lpThreadAttributes,
+BOOL CThread::SetParameter(LPSECURITY_ATTRIBUTES lpThreadAttributes,
 	DWORD dwStackSize,
 	LPTHREAD_START_ROUTINE lpStartAddress,
 	LPVOID lpParameter,
 	DWORD dwCreationFlags)
 {
+	if (lpStartAddress == NULL) {
+		return FALSE;
+	}
+
 	m_lpThreadAttributes = lpThreadAttributes;
 	m_dwStackSize = dwStackSize;
 	m_lpStartAddress = lpStartAddress;
 	m_lpParameter = lpParameter;
 	m_dwCreationFlags = dwCreationFlags;
 
-	//if (m_lpStartAddress == NULL) {
-	//	return FALSE;
-	//}
-	//m_hThread = ::CreateThread(
-	//	m_lpThreadAttributes,
-	//	m_dwStackSize,
-	//	m_lpStartAddress,
-	//	m_lpParameter,
-	//	m_dwCreationFlags,
-	//	&m_dwThreadId);
-	//return (m_hThread != NULL);
 	return TRUE;
 }
+
 BOOL CThread::Start()
 {
 	if (m_lpStartAddress == NULL) {
@@ -97,31 +91,46 @@ BOOL CThread::Start()
 	::ResumeThread(m_hThread);
 	return TRUE;
 }
-BOOL CThread::Join()
+
+BOOL CThread::Join(DWORD dwWait/*=INFINITE*/)
 {
-	BOOL bClose = FALSE;
-	::WaitForSingleObject(m_hThread, INFINITE);
-	bClose &= ::CloseHandle(m_hThread);
-	return bClose;
+	::WaitForSingleObject(m_hThread, dwWait);
+	::CloseHandle(m_hThread);
+	return TRUE;
 }
 
-BOOL CThread::JoinAll(CThread acThread[], int nCount)
+BOOL CThread::JoinAll(CThread acThread[], int nCount, DWORD dwWait/*=INFINITE*/)
 {
-	HANDLE ahThread[10];		// TODO: ÉXÉåÉbÉhÇÃå¬êîÇ…ëŒâû
+	if (acThread == NULL) {
+		return FALSE;
+	}
+
+	HANDLE* pHandle = new HANDLE[nCount];
+	if (pHandle == NULL) {
+		return FALSE;
+	}
 
 	for (int i = 0; i < nCount; i++) {
-		ahThread[i] = acThread[i].GetHandle();
+		pHandle[i] = acThread[i].GetHandle();
 	}
 
 	::WaitForMultipleObjects(
 		nCount,
-		ahThread,
+		pHandle,
 		TRUE,
-		INFINITE);
+		dwWait);
+
+	for (int i = 0; i < nCount; i++) {
+		::CloseHandle(pHandle[i]);
+	}
+
+
+	delete[] pHandle;
+
 	return TRUE;
 }
 
-BOOL CThread::SetFunction(LPTHREAD_START_ROUTINE lpStartAddress)
+BOOL CThread::SetThreadProc(LPTHREAD_START_ROUTINE lpStartAddress)
 {
 	if (lpStartAddress == NULL) {
 		return FALSE;
@@ -129,7 +138,8 @@ BOOL CThread::SetFunction(LPTHREAD_START_ROUTINE lpStartAddress)
 	m_lpStartAddress = lpStartAddress;
 	return TRUE;
 }
-BOOL CThread::SetParameter(LPVOID lpParameter)
+
+BOOL CThread::SetThreadParam(LPVOID lpParameter)
 {
 	if (lpParameter == NULL) {
 		return FALSE;
@@ -142,3 +152,36 @@ HANDLE CThread::GetHandle()
 {
 	return m_hThread;
 }
+
+
+// *** sample ***
+//DWORD WINAPI MyThreadFunction(LPVOID lpParam)
+//{
+//	HANDLE hStdout;
+//	PMYDATA pDataArray;
+//
+//	TCHAR msgBuf[BUF_SIZE];
+//	size_t cchStringSize;
+//	DWORD dwChars;
+//
+//	// Make sure there is a console to receive output results. 
+//
+//	hStdout = GetStdHandle(STD_OUTPUT_HANDLE);
+//	if (hStdout == INVALID_HANDLE_VALUE)
+//		return 1;
+//
+//	// Cast the parameter to the correct data type.
+//	// The pointer is known to be valid because 
+//	// it was checked for NULL before the thread was created.
+//
+//	pDataArray = (PMYDATA)lpParam;
+//
+//	// Print the parameter values using thread-safe functions.
+//
+//	StringCchPrintf(msgBuf, BUF_SIZE, TEXT("Parameters = %d, %d\n"),
+//		pDataArray->val1, pDataArray->val2);
+//	StringCchLength(msgBuf, BUF_SIZE, &cchStringSize);
+//	WriteConsole(hStdout, msgBuf, (DWORD)cchStringSize, &dwChars, NULL);
+//
+//	return 0;
+//}
